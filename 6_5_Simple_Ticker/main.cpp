d4dc9fbef3e7cfc61965591bc399d0764a88467f
KL-Huang <craint1001@gmail.com>
2021-03-28 14:32:23 -0700

    commit

--- 6_5_Simple_Ticker/main.cpp
+++ 6_5_Simple_Ticker/main.cpp
@@ -2,6 +2,23 @@
 using namespace std::chrono;
 
 Ticker flipper;
+DigitalOut led3(LED3);
+
+void flip()
+{
+   led3 = !led3;
+}
+
+int main()
+{
+   led3 = 1;
+   flipper.attach(&flip, 500ms); // the address of the function to be attached (flip) and the interval (2 seconds)
+}
+/*
+#include "mbed.h"
+using namespace std::chrono;
+
+Ticker flipper;
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 
@@ -22,3 +39,4 @@ int main()
       ThisThread::sleep_for(200ms);
    }
 }
+*/
